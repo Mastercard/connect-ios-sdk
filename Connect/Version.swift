42cb922d0edd4eeef25692d51652b1ebe74c7a61
Prathamesh Salawkar <prathamesh.salawkar@finicity.com>
2022-08-23 11:49:12 +0530

    MR feedback fix added for lowercase version details.

--- Connect/Version.swift
+++ Connect/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public func sdkVersion() -> String {
     var version: String?
-    if let dict = Bundle(identifier: "com.MastercardOpenBanking.Connect")?.infoDictionary {
+    if let dict = Bundle(identifier: "com.mastercard.openbanking.connect")?.infoDictionary {
         version = dict["CFBundleShortVersionString"] as? String
     }
     return version ?? "2.0.0"
